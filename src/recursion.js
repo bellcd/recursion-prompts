1c0596a946d48ca6ab3d86b0007820969e832cd2
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-07-16 22:49:01 -0400

    Implements augmentElements function

--- src/recursion.js
+++ src/recursion.js
@@ -665,6 +665,12 @@ var compress = function(list) {
 // itself.
 // augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
 var augmentElements = function(array, aug) {
+  // base case, array is empty, return []
+  // recursive case
+    // return [first element, concatted with aug] concatted with recursive call to array sliced from sub 1
+  if (array.length === 0) return [];
+  return [array[0].concat(aug)].concat(augmentElements(array.slice(1), aug));
+
 };
 
 // 34. Reduce a series of zeroes to a single 0.
